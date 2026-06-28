d0929440fbc961aecc13b5591ff2a8f2bb8d4e8e
joaomota27 <motajoao14@gmail.com>
2026-06-28 17:19:50 -0300

    Updated tracking-config.ts

--- src/tracking-config.ts
+++ src/tracking-config.ts
@@ -1,3 +1,6 @@
 export const TRACKING_CONFIG = {
-  facebookPixelId: "COLOCAR_PIXEL_AQUI",
+  facebookPixelId: "PIXEL_ID_1",
+  facebookPixelId2: "PIXEL_ID_2",
+  facebookPixelId3: "PIXEL_ID_3",
+  facebookPixelId4: "PIXEL_ID_4",
 };

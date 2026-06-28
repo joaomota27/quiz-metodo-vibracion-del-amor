7ac2dc003ab31371d1fe8d71fce579b262301be1
joaomota27 <motajoao14@gmail.com>
2026-06-28 16:45:41 -0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { useState, useCallback, useEffect } from 'react';
 
- const removeFloating = () => {
-document.querySelectorAll('[style="position: fixed"][style="bottom: 1rem"][style="right: 1rem"][style="z-index: 2147483647"]').forEach(el => el.remove());
-};
-
-
+  const removeFloating = () => {
+  document.querySelectorAll('[style="position: fixed"][style="bottom: 1rem"][style="right: 1rem"][style="z-index: 2147483647"]').forEach(el => el.remove());};
 
 // executa já no load
 removeFloating();
